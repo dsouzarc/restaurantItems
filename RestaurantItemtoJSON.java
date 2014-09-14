a88a3b26311d070ca32bc06776a61436031905a5
Ryan D'souza <dsouzarc@gmail.com>
2014-09-14 12:52:03 -0400

    Gets item title, description, and cost, makes json object

--- RestaurantItemtoJSON.java
+++ RestaurantItemtoJSON.java
@@ -20,6 +20,36 @@ public class RestaurantItemtoJSON {
     try { 
       final File theFile = new File(restaurantName + ".txt");
       
+      final JSONObject restaurant = new JSONObject();
+      final JSONArray restaurantItems = new JSONArray();
+      
+      boolean toContinue = true;
+      
+      while(toContinue) { 
+        System.out.println("Enter item name: ");
+        final String itemName = theScanner.nextLine();
+        
+        if(itemName.equals("end")) { 
+          toContinue = false;
+          break;
+        }
+        
+        System.out.println("Item cost: ");
+        final double itemCost = 
+          Double.parseDouble(theScanner.nextLine().replace("$", "").replace("+", ""));
+        
+        System.out.println("Item description: ");
+        final String itemDescription = theScanner.nextLine();
+        
+        final JSONObject restaurantItem = new JSONObject();
+        restaurantItem.put("name", itemName);
+        restaurantItem.put("price", String.valueOf(itemCost));
+        restaurantItem.put("description", itemDescription);
+        
+        restaurantItems.put(restaurantItem);
+        
+      }
+      
     }
     catch(Exception e) { 
       e.printStackTrace();
